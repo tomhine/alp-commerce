ee95dc1b5a0043a72d90ddc4818b0120bd3a6d85
Tom Hine <tomhine792@gmail.com>
2021-08-18 11:31:52 +0100

    add footer to layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,3 +1,4 @@
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import styles from './Layout.module.scss';
 
@@ -6,6 +7,7 @@ const Layout = ({ children }) => {
     <div className={styles.app}>
       <Header />
       <main className={styles.main}>{children}</main>
+      <Footer />
     </div>
   );
 };
