a7b2f49a1026b4e01f902bd0d731f7ad7228ad7c
Tom Hine <tomhine792@gmail.com>
2021-08-18 16:27:42 +0100

    remove footer

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import Footer from '../Footer/Footer';
+// import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import styles from './Layout.module.scss';
 
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     <div className={styles.app}>
       <Header />
       <main className={styles.main}>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
