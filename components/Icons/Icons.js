d6d87b47c50f40e90a46da26993d41f0f9879b2e
Tom Hine <tomhine792@gmail.com>
2021-08-18 11:30:44 +0100

    add heart icon

--- components/Icons/Icons.js
+++ components/Icons/Icons.js
@@ -65,3 +65,24 @@ export const MenuIcon = props => {
     </svg>
   );
 };
+
+export const HeartIcon = props => {
+  return (
+    <svg
+      style={iconStyle}
+      xmlns="http://www.w3.org/2000/svg"
+      height={props.size || defaultSize}
+      width={props.size || defaultSize}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke={props.color || 'currentColor'}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+      />
+    </svg>
+  );
+};
