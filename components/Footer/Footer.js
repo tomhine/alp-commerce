c225c622e73878fd58d8ac75b00b7f5408c80c17
Tom Hine <tomhine792@gmail.com>
2021-08-18 11:30:28 +0100

    add footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span>alp ltd</span>
+    </footer>
+  );
+};
+
+export default Footer;
