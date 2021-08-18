21be8e4613810f851829911c46627425a7dd4184
Tom Hine <tomhine792@gmail.com>
2021-08-18 16:27:25 +0100

    add links for logo and menu

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,20 +3,29 @@ import { HeartIcon, MenuIcon, SearchIcon, ShoppingBagIcon } from '../Icons/Icons
 import Logo from '../Logo/Logo';
 import SearchBar from '../SearchBar/SearchBar';
 import styles from './Header.module.scss';
+import Link from 'next/link';
 
 const Header = () => {
   const winWidth = useWindowSize().width;
 
   return (
     <header className={styles.header}>
-      <Logo />
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
       {winWidth >= 1024 && <SearchBar />}
       <span className={styles.actions}>
         {winWidth < 768 && <SearchIcon />}
         {winWidth >= 768 && winWidth < 1024 && <SearchBar />}
         <HeartIcon />
         <ShoppingBagIcon />
-        <MenuIcon />
+        <Link href="/menu">
+          <a>
+            <MenuIcon />
+          </a>
+        </Link>
       </span>
     </header>
   );
