d1d16d2da09483ea45a474f7af6bdc927fa26fcf
Tom Hine <tomhine792@gmail.com>
2021-08-18 11:31:16 +0100

    get window with for conditionals on search

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,15 +1,22 @@
-import { MenuIcon, SearchIcon, ShoppingBagIcon } from '../Icons/Icons';
+import useWindowSize from '../../hooks/useWindowSize';
+import { HeartIcon, MenuIcon, SearchIcon, ShoppingBagIcon } from '../Icons/Icons';
 import Logo from '../Logo/Logo';
+import SearchBar from '../SearchBar/SearchBar';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const winWidth = useWindowSize().width;
+
   return (
     <header className={styles.header}>
       <Logo />
+      {winWidth >= 1024 && <SearchBar />}
       <span className={styles.actions}>
-        <SearchIcon color="#fff" />
-        <ShoppingBagIcon color="#fff" />
-        <MenuIcon color="#fff" />
+        {winWidth < 768 && <SearchIcon />}
+        {winWidth >= 768 && winWidth < 1024 && <SearchBar />}
+        <HeartIcon />
+        <ShoppingBagIcon />
+        <MenuIcon />
       </span>
     </header>
   );
